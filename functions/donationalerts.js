90c218d848c2c8afc73bcfc041b17b928bdf526e
Banonevar <me@banonevar.tk>
2020-06-01 16:41:38 +0300

    Add donationalerts token generator (#6)

---
+++ functions/donationalerts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const axios = require('axios');
+
+exports.handler = async function(event, context, callback) {
+  const clientId = process.env.DONATIONALERTS_CLIENTID;
+  const clientSecret = process.env.DONATIONALERTS_CLIENTSECRET;
+  const code = event.queryStringParameters.code;
+
+  if (!code) {
+    callback(null, {
+      statusCode: 400,
+      body: 'Missing code'
+    });
+  }
+
+  try {
+    const { data } = await axios.post('https://www.donationalerts.com/oauth/token', {
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: 'http://sogebot.xyz/integrations/donationalerts/code/',
+      code
+    });
+
+    callback(null, {
+      statusCode: 200,
+      body: data.access_token
+    });
+  } catch (e) {
+    callback(null, {
+      statusCode: 400,
+      body: 'Invalid code'
+    });
+  }
+};
